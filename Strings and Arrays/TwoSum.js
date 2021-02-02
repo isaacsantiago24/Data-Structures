e7f6d2816479d6e46f0bf1db4bda2bacfcdbea61
Isaac Santiago <62231878+isaacsantiago24@users.noreply.github.com>
2021-02-01 20:52:03 -0800

    added second solution

--- Strings and Arrays/TwoSum.js
+++ Strings and Arrays/TwoSum.js
@@ -21,3 +21,20 @@ function twoSum(nums, target) {
 }
 
 twoSum([1, 3, 4], 5); //[0,2]
+
+//Second solution
+
+const twoSum_Solution = (arr, target) => {
+  let numObject = {};
+  for (let i = 0; i < arr.length; i++) {
+    let thisNum = arr[i];
+    numObject[thisNum] = i;
+  }
+  for (var i = 0; i < arr.length; i++) {
+    let diff = target - arr[i];
+    if (numObject.hasOwnProperty(diff) && numObject[diff] !== i) {
+      return [i, numObject[diff]];
+    }
+  }
+};
+console.log(twoSum_Solution([2, 7, 11, 15], 9));
